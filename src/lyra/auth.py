5a8e75b9a21a26cdab1039c84cfe90742063d319
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2026-04-14 14:21:17 -0600

    Formatting

--- src/lyra/auth.py
+++ src/lyra/auth.py
@@ -1,12 +1,13 @@
 import ee
 from google.oauth2.service_account import Credentials
 
-KEY_FILE = '/app/service-account.json'
-SCOPES = ['https://www.googleapis.com/auth/earthengine']
 
 def initialize_earth_engine():
     try:
-        credentials = Credentials.from_service_account_file(KEY_FILE, scopes=SCOPES)
+        credentials = Credentials.from_service_account_file(
+            "/app/service-account.json",
+            scopes=["https://www.googleapis.com/auth/earthengine"],
+        )
         ee.Initialize(credentials)
         print("Earth Engine initialized successfully!")
     except Exception as e:

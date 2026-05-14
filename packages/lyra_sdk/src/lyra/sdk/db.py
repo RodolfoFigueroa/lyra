885c511c9777a1d467771e7919259fae2833b969
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2026-05-14 12:06:38 -0600

    Formatting

--- packages/lyra_sdk/src/lyra/sdk/db.py
+++ packages/lyra_sdk/src/lyra/sdk/db.py
@@ -1,14 +1,24 @@
 from abc import ABC, abstractmethod
+from typing import Literal
 
 from lyra.sdk.models import GeoJSON
 
 
 class LyraDB(ABC):
     @abstractmethod
-    def load_denue(self, data: GeoJSON, *, year: int) -> GeoJSON: ...
+    def load_denue(
+        self,
+        data: GeoJSON,
+        *,
+        year: Literal[2020, 2021, 2022, 2023, 2024, 2025],
+        month: Literal[5, 11],
+        buffer_size: float = 10_000,
+    ) -> GeoJSON: ...
 
     @abstractmethod
-    def load_mesh(self, data: GeoJSON, *, buffer_size: float = 10000) -> GeoJSON: ...
+    def load_mesh(
+        self, data: GeoJSON, *, buffer_size: float = 10_000, level: Literal[9] = 9,
+    ) -> GeoJSON: ...
 
     @abstractmethod
     def load_census(
@@ -16,5 +26,6 @@ class LyraDB(ABC):
         data: GeoJSON,
         *,
         columns: list[str],
-        buffer_size: float = 10000,
+        year: Literal[2010, 2020] = 2020,
+        buffer_size: float = 10_000,
     ) -> GeoJSON: ...

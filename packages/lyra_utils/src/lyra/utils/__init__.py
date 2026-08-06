f2f557b1e4e27e65c7c0f2c8df0e9de722836255
RodolfoFigueroa <rodolfo@daku.moe>
2026-08-06 14:37:47 -0600

    fix: Fix stale imports

--- packages/lyra_utils/src/lyra/utils/__init__.py
+++ packages/lyra_utils/src/lyra/utils/__init__.py
@@ -2,24 +2,18 @@
 
 from lyra.utils.date import get_date_range, get_season_date_range
 from lyra.utils.ee import (
-    chunk_gdf,
-    compute_gdf,
     convert_gdf_to_ee,
     convert_polygon_to_ee,
-    get_reducer_name,
-    reduce_ee_image_over_gdf_factory,
+    reduce_ee_image_over_gdf,
 )
 from lyra.utils.geometry import calculate_feature_areas_m2, convert_geojson_to_gdf
 
 __all__ = [
     "calculate_feature_areas_m2",
-    "chunk_gdf",
-    "compute_gdf",
     "convert_gdf_to_ee",
     "convert_geojson_to_gdf",
     "convert_polygon_to_ee",
     "get_date_range",
-    "get_reducer_name",
     "get_season_date_range",
-    "reduce_ee_image_over_gdf_factory",
+    "reduce_ee_image_over_gdf",
 ]
